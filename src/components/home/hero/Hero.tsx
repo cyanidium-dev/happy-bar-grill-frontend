7ca92dd99087099d27f0291b09a81707c18ad3fd
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-08-06 21:03:05 +0300

    added desktop styles

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -1,5 +1,4 @@
 import { getTranslations } from "next-intl/server";
-import AnimatedWrapper from "@/components/shared/animatedWrappers/AnimatedWrapper";
 import Button from "@/components/shared/buttons/Button";
 import Container from "@/components/shared/container/Container";
 import HeroDishCard from "@/components/home/hero/HeroDishCard";
@@ -41,28 +40,24 @@ export default async function Hero() {
       </div>
       <Container className="relative items-center gap-10">
         <div className="flex min-w-0 flex-col gap-7 mb-[140px] xs:mb-[91px]">
-          <AnimatedWrapper animation={{ x: -40 }} className="relative -z-15">
+          <div className="relative -z-15">
             <PageTitle className="max-w-[301px] sm:max-w-[570px]">
               {t("title")}
             </PageTitle>
-          </AnimatedWrapper>
+          </div>
 
           <div className="flex flex-col gap-10 lg:flex-row-reverse lg:justify-between lg:items-center lg:max-w-[460px]">
             {" "}
-            <AnimatedWrapper animation={{ x: 40 }}>
-              <p className="max-w-[200px] lg:max-w-[181px] mb-1 text-12light text-white">
-                {t("description")}
-              </p>
-            </AnimatedWrapper>
-            <AnimatedWrapper animation={{ x: -40 }}>
-              <Button
-                href="/menu"
-                size="lg"
-                className="w-full xs:w-fit uppercase text-12bold font-findsans text-white"
-              >
-                {t("cta")}
-              </Button>
-            </AnimatedWrapper>
+            <p className="max-w-[200px] lg:max-w-[181px] mb-1 text-12light text-white">
+              {t("description")}
+            </p>
+            <Button
+              href="/menu"
+              size="lg"
+              className="w-full xs:w-fit uppercase text-12bold font-findsans text-white"
+            >
+              {t("cta")}
+            </Button>
           </div>
         </div>
 
@@ -100,10 +95,7 @@ export default async function Hero() {
           />
         </div>
       </Container>
-      <AnimatedWrapper
-        animation={{ y: 24 }}
-        className="min-w-0 xs:max-w-full lg:max-w-[1024px] xl:max-w-[1280px] lg:px-20 sm:ml-[calc(50%-320px)] md:ml-[calc(50%-384px)] lg:ml-[calc(50%-512px)] xl:ml-[calc(50%-640px)] mx-auto"
-      >
+      <div className="min-w-0 xs:max-w-full lg:max-w-[1024px] xl:max-w-[1280px] lg:px-20 sm:ml-[calc(50%-320px)] md:ml-[calc(50%-384px)] lg:ml-[calc(50%-512px)] xl:ml-[calc(50%-640px)] mx-auto">
         <ul className="flex min-w-0 gap-3 overflow-x-auto pl-6 lg:pl-0 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
           {featuredDishes.map((dish) => (
             <li key={dish.slug}>
@@ -111,7 +103,7 @@ export default async function Hero() {
             </li>
           ))}
         </ul>
-      </AnimatedWrapper>
+      </div>
     </section>
   );
 }

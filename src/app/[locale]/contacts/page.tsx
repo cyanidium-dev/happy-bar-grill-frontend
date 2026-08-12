13327e9221b4eee2ad41462c1f500cf68f30f510
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-08-12 15:50:32 +0300

    updated contacts page

--- src/app/[locale]/contacts/page.tsx
+++ src/app/[locale]/contacts/page.tsx
@@ -32,6 +32,9 @@ export default async function ContactsPage({ params }: PageProps) {
 
       <section className="relative z-10 overflow-hidden bg-white">
         <Container className="relative pb-16 pt-10 md:pb-20 md:pt-14">
+          <div className="hidden xl:block absolute -z-10 bg-navy-dark top-140 left-[110px] sm:w-[321px] sm:h-[303px] rounded-full" />
+          <div className="hidden xl:block absolute z-10 bg-red top-160 left-[190px] sm:w-[321px] sm:h-[303px] rounded-full" />
+
           <div className="absolute -z-10 bg-navy-dark bottom-0 right-[-30px] sm:bottom-[-74px] sm:right-[-69px] w-[240px] h-[227px] sm:w-[481px] sm:h-[454px] rounded-full" />
           <div className="absolute z-10 bg-red bottom-[-60px] right-[-90px] sm:bottom-[-210px] sm:right-[-170px] w-[240px] h-[227px] sm:w-[481px] sm:h-[454px] rounded-full" />
 
@@ -45,7 +48,7 @@ export default async function ContactsPage({ params }: PageProps) {
           </div>
 
           <div className="relative -z-10 grid gap-8 lg:grid-cols-2 lg:items-start lg:gap-10">
-            <div
+            {/* <div
               aria-hidden
               className="hidden xl:block pointer-events-none absolute -z-10 -bottom-26 -left-40 w-[600px] h-[226px]"
             >
@@ -55,12 +58,12 @@ export default async function ContactsPage({ params }: PageProps) {
                 fill
                 className="object-cover"
               />
-            </div>
+            </div> */}
             <ContactInfo />
             <ContactForm />
           </div>
 
-          <div className="mt-10 md:mt-14">
+          <div className="z-15 relative mt-10 md:mt-14">
             <ContactsMap />
           </div>
           <div aria-hidden className={FOOTER_WAVE_HEIGHT_CLASS} />
